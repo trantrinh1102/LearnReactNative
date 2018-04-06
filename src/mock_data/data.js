6ac9f6c4c449355ba574a17a0f73e4b13a8020b4
tran.cong.trinh <food.drink.framgia@gmail.com>
2018-04-06 13:03:49 +0700

    Add List

---
+++ src/mock_data/data.js
@@ -0,0 +1,27 @@
+export const users = [
+  {
+    name: 'brynn',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+  },
+  {
+    name: 'thot leader',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/evagiselle/128.jpg',
+  },
+  {
+    name: 'jsa',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg',
+  },
+  {
+    name: 'talhaconcepts',
+    avatar:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/talhaconcepts/128.jpg',
+  },
+  {
+    name: 'andy vitale',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/andyvitale/128.jpg',
+  },
+  {
+    name: 'katy friedson',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg',
+  },
+];
